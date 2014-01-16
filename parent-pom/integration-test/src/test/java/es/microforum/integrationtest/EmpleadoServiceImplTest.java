538467f36beed52b920acd394ebfa909fc874794
ClickApp <Jurosa24@gmail.com>
2014-01-16 21:44:40 +0100

    Subo el Test de Integración de Empleado

--- parent-pom/integration-test/src/test/java/es/microforum/integrationtest/EmpleadoServiceImplTest.java
+++ parent-pom/integration-test/src/test/java/es/microforum/integrationtest/EmpleadoServiceImplTest.java
@@ -6,53 +6,78 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import es.microforum.model.Empleado;
 import es.microforum.serviceapi.EmpleadoService;
-import es.microforum.serviceimpl.EmpleadoServiceImpl;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring-data-app-context.xml")
+@TransactionConfiguration(defaultRollback=true)
 public class EmpleadoServiceImplTest {
 	
 	GenericXmlApplicationContext ctx;
+	@Autowired
 	EmpleadoService empleadoService;
 	Empleado empleado;
-	
+		
 	@Before
-	public void setUp() throws Exception {
-		ctx = new GenericXmlApplicationContext();
-		ctx.load("classpath:spring-data-app-context.xml");
-		ctx.refresh();		
-		empleado = new Empleado("1234");
-		empleadoService = ctx.getBean("springJpaEmpleadoService", EmpleadoService.class);
-		//empleadoService.delete(empleado);
-		//empleadoService.save(empleado);
+	public void setUp() throws Exception {		
+		empleado = new Empleado();	
 	}
 
 	@Test
+	@Transactional
 	public void testFindAll() {				
+		empleado.setDni("123");
+		empleadoService.save(empleado);				
 		List<Empleado> empleados = empleadoService.findAll();
-		assertTrue(empleados.size() > 0);
+		assertTrue(empleados.contains(empleado));
 	}
 
-//	@Test
-//	public void testFindByDni() {
-//		
-//	}
-//
-//	@Test
-//	public void testFindByNombre() {
-//		
-//	}
-//
-//	@Test
-//	public void testSave() {
-//		
-//	}
-//
-//	@Test
-//	public void testDelete() {
-//		
-//	}
+	@Test
+	@Transactional
+	public void testFindByDni() {		
+		empleado.setDni("123");
+		empleadoService.save(empleado);
+		empleado = empleadoService.findByDni("123");
+		assertTrue(empleado.getDni().equals("123"));		
+	}
+
+	@Test
+	@Transactional
+	public void testFindByNombre() {		
+		empleado.setDni("123");
+		empleado.setNombre("Javier");
+		empleadoService.save(empleado);
+		List<Empleado> empleados = empleadoService.findByNombre("Javier");	
+		assertTrue(empleados.contains(empleado));
+	}
+
+	@Test
+	@Transactional
+	public void testSave() {
+		empleado.setDni("123");
+		empleadoService.save(empleado);
+		empleado = empleadoService.findByDni("123");
+		assertTrue(empleado.getDni().equals("123"));
+	}
+
+	@Test
+	@Transactional
+	public void testDelete() {
+		empleado.setDni("123");
+		empleadoService.save(empleado);
+		empleado = empleadoService.findByDni("123");
+		assertTrue(empleado.getDni().equals("123"));
+		empleadoService.delete(empleado);
+		assertTrue(!empleadoService.findAll().contains(empleado));
+	}
 
 }
